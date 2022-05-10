8d47cca24ef89a516f9c7f0810fb5c8e6f87c7ef
vyvux <vuhtvy@gmail.com>
2022-05-10 08:29:20 +0700

    add doctor schedule by medical staff base components

--- reactjs/src/containers/System/Staff/StaffScheduleManage.js
+++ reactjs/src/containers/System/Staff/StaffScheduleManage.js
@@ -3,212 +3,149 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Label, Input, Col, Container, InputGroup, InputGroupText } from "reactstrap";
 import "../UserManage.scss";
+import "./StaffScheduleManage.scss";
 import { toast } from "react-toastify";
+import * as actions from "../../../store/actions";
 // import { values } from "lodash";
 
 class StaffScheduleManage extends Component {
-  // state = {};
   constructor(props) {
     super(props);
     this.state = {
-      arrUsers: [],
-      filteredUserList: [],
-      isOpenModalUser: false,
-      isOpenModalDeleteUserConfirm: false,
-      isOpenModalEditUser: false,
-      userInEffect: {},
-      query: "",
-      role: "",
+      doctorId: "",
+      date: "",
+      arrSchedule: [],
     };
   }
 
   async componentDidMount() {
-    // this.getAllUsersFromDB();
+    this.props.getDoctorStart();
+    this.props.getTimeStart();
   }
 
-  //   getAllUsersFromDB = async () => {
-  //     let response = await getAllUsers("ALL");
-  //     if (response && response.errCode === 0) {
-  //       this.setState({
-  //         arrUsers: response.users,
-  //         filteredUserList: response.users,
-  //       });
-  //     }
-  //   };
+  handleOnChangeInput = (event, id) => {
+    let copyState = { ...this.state };
+    copyState[id] = event.target.value;
+    this.setState({
+      ...copyState,
+    });
+  };
 
-  //   handleAddNewUser = () => {
-  //     this.setState({
-  //       isOpenModalUser: true,
-  //     });
-  //   };
-
-  //   toggleUserModal = () => {
-  //     this.setState({
-  //       isOpenModalUser: !this.state.isOpenModalUser,
-  //     });
-  //   };
-
-  //   createNewUser = async (data) => {
-  //     let success = false;
-  //     try {
-  //       let response = await createNewUserByAdmin(data);
-  //       if (response && response.errCode !== 0) {
-  //         toast.error(response.errMessage);
-  //       } else {
-  //         await this.getAllUsersFromDB();
-  //         this.setState({
-  //           isOpenModalUser: false,
-  //         });
-  //         success = true;
-  //         toast.success(response.message);
-  //       }
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //     return success;
-  //   };
-
-  //   toggleModalDeleteUserConfirm = () => {
-  //     this.setState({
-  //       isOpenModalDeleteUserConfirm: !this.state.isOpenModalDeleteUserConfirm,
-  //     });
-  //   };
-
-  //   openDeleteConfirmModal = (user) => {
-  //     this.setState({
-  //       isOpenModalDeleteUserConfirm: true,
-  //       userInEffect: user,
-  //     });
-  //   };
-
-  //   handleDeleteUser = async (user) => {
-  //     try {
-  //       let response = await deleteUserByAdmin(user.id);
-  //       if (response && response.errCode !== 0) {
-  //         toast.error(response.errMessage, { autoClose: 10000 });
-  //       } else {
-  //         await this.getAllUsersFromDB();
-  //         toast.success(response.message);
-  //       }
-  //       this.setState({
-  //         isOpenModalDeleteUserConfirm: false,
-  //       });
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-
-  //   toggleModalEditUser = () => {
-  //     this.setState({
-  //       isOpenModalEditUser: !this.state.isOpenModalEditUser,
-  //     });
-  //   };
-
-  //   openEditUserModal = (user) => {
-  //     this.setState({
-  //       isOpenModalEditUser: true,
-  //       userInEffect: user,
-  //     });
-  //   };
-
-  //   handleEditUser = async (user) => {
-  //     try {
-  //       let response = await editUserByAdmin(user);
-  //       if (response && response.errCode === 0) {
-  //         await this.getAllUsersFromDB();
-  //         toast.success(response.message);
-  //       } else {
-  //         toast.error(response.errMessage);
-  //       }
-  //       this.setState({
-  //         isOpenModalEditUser: false,
-  //       });
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-
-  //   handleOnChangeInput = (event, id) => {
-  //     let copyState = { ...this.state };
-  //     copyState[id] = event.target.value;
-  //     this.setState(
-  //       {
-  //         ...copyState,
-  //       },
-  //       () => {
-  //         this.handleFilterUser();
-  //       }
-  //     );
-  //   };
-
-  //   checkRole = (user) => {
-  //     let role = this.state.role;
-  //     if (role) {
-  //       return user.roleId === this.state.role;
-  //     }
-  //     return true;
-  //   };
-
-  //   checkTerm = (user) => {
-  //     let term = this.state.query.toLowerCase();
-  //     if (term) {
-  //       return user.email.toLowerCase().includes(term) || user.firstName.toLowerCase().includes(term) || user.lastName.toLowerCase().includes(term);
-  //     }
-  //     return true;
-  //   };
-
-  //   handleFilterUser = () => {
-  //     this.setState({
-  //       filteredUserList: this.state.arrUsers.filter((user) => {
-  //         return this.checkRole(user) && this.checkTerm(user);
-  //       }),
-  //     });
-  //   };
+  setToday = () => {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = `0${dd}`;
+    }
+    if (mm < 10) {
+      mm = `0${mm}`;
+    }
+    return `${yyyy}-${mm}-${dd}`;
+  };
 
   render() {
-    // let arrUsers = this.state.arrUsers;
-    let filteredUserList = this.state.filteredUserList;
-
-    const renderRole = (roleId) => {
-      switch (roleId) {
-        case "R1":
-          return "Admin";
-        case "R2":
-          return "Doctor";
-        case "R3":
-          return "Patient";
-        case "R4":
-          return "Medical Staff";
-        default:
-          return "Unknown role";
-      }
-    };
+    let doctorList = this.props.doctors;
+    let timeList = this.props.time;
+    console.log(timeList);
 
     return (
-      <div className="users-container mx-1">
-        {/* <ModalUser isOpen={this.state.isOpenModalUser} toggleModalFromParent={this.toggleUserModal} createNewUser={this.createNewUser} />
-
-        <ModalDeleteUserConfirm
-          isOpen={this.state.isOpenModalDeleteUserConfirm}
-          toggleModalFromParent={this.toggleModalDeleteUserConfirm}
-          deleteUserByAdmin={this.handleDeleteUser}
-          user={this.state.userInEffect}
-        />
-
-        <ModalEditUser isOpen={this.state.isOpenModalEditUser} toggleModalFromParent={this.toggleModalEditUser} editUserByAdmin={this.handleEditUser} user={this.state.userInEffect} /> */}
-
+      <div className="schedule-manage-container">
         <div className="title text-center">Doctor Schedule Manage</div>
+
+        <div className="container">
+          <div className="row g-2">
+            {/* Doctor select */}
+            <div className="col-md-6 col-sm-12  form-group">
+              <div className="input-group mb-3">
+                <label className="input-group-text" htmlFor="doctorSelect">
+                  Select Doctor
+                </label>
+                <select
+                  className="form-select"
+                  id="doctorSelect"
+                  onChange={(event) => {
+                    this.handleOnChangeInput(event, "doctorId");
+                  }}
+                  value={this.state.doctorId}
+                >
+                  <option value="">Choose...</option>
+                  {doctorList &&
+                    doctorList.map((doctor, index) => {
+                      return (
+                        <option value={doctor.id} key={doctor.id}>
+                          Doctor ID {doctor.id} - {doctor.firstName} {doctor.lastName}
+                        </option>
+                      );
+                    })}
+                </select>
+              </div>
+            </div>
+
+            {/* Date select */}
+            <div className="col-md-6 col-sm-12  form-group">
+              <div className="input-group mb-3">
+                <label className="input-group-text" htmlFor="dateSelect">
+                  Select Date
+                </label>
+                <input
+                  type="date"
+                  className="form-select"
+                  min={this.setToday()}
+                  id="dateSelect"
+                  onChange={(event) => {
+                    this.handleOnChangeInput(event, "date");
+                  }}
+                  value={this.state.date}
+                />
+              </div>
+            </div>
+
+            {/* Pick doctor hours */}
+            <div className="col-12 pick-hours-container">
+              <label>Select available hours</label>
+              <div className="hours">
+                <div className="row">
+                  {timeList &&
+                    timeList.map((item, index) => {
+                      return (
+                        <div className="col-12 col-md-3" key={item.key}>
+                          <button className="btn btn-info">{item.value}</button>
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
+            </div>
+
+            {/* Save doctor schedule button */}
+            <div className="col-12 d-flex justify-content-center">
+              <button
+                className="btn btn-success px-3"
+                onClick={() => {
+                  console.log("check schedule state: ", this.state);
+                }}
+              >
+                Save Schedule
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return {};
+  return { doctors: state.doctor.doctors, time: state.code.time };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    getDoctorStart: () => dispatch(actions.fetchDoctorStart()),
+    getTimeStart: () => dispatch(actions.fetchTimeStart()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(StaffScheduleManage);
